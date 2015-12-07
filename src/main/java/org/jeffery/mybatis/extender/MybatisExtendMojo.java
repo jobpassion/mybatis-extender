01e48679fcecf5b73f6fec1dce36ada120fe032f
jobpassion@gmail.com <261103692>
2015-12-07 12:55:09 +0800

    a

--- src/main/java/org/jeffery/mybatis/extender/MybatisExtendMojo.java
+++ src/main/java/org/jeffery/mybatis/extender/MybatisExtendMojo.java
@@ -54,7 +54,6 @@ public class MybatisExtendMojo extends AbstractMojo {
     }
     private void processResourcesDir(String filename){
         File file = new File(filename);
-        System.out.println(file.getAbsoluteFile());
         if(file.isDirectory()){
             for(File f:file.listFiles()){
                 processResourcesDir(f.getAbsolutePath());
@@ -99,6 +98,7 @@ public class MybatisExtendMojo extends AbstractMojo {
             header = header.replaceAll("extend=\"([^\"]*)\"","");
             parsed = parsed.replace(header, header + "\n" + superContent);
             FileUtils.writeStringToFile(new File(project.getFile().getParent() + "/target/classes" + filename.substring(filename.indexOf("resources") + 9)), parsed);
+            System.out.println("mybatis extender: succeed:" + filename);
         } catch (Exception e) {
             System.out.println("mybatis extender: error process file:" + filename);
 //            e.printStackTrace();
